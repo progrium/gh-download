27da2bc59298f19ecbfeeb9197d8551885537e38
Jeff Lindsay <progrium@gmail.com>
2015-02-10 18:46:12 -0600

    fixed passing http status on download, added latest version endpoint

---
+++ README.md
@@ -0,0 +1,5 @@
+todo
+
+ * download badge
+ * latest redirect
+ * changelog text? redirect?
--- gh-download.go
+++ gh-download.go
@@ -11,6 +11,7 @@ import (
 
 	"code.google.com/p/goauth2/oauth"
 	"github.com/google/go-github/github"
+	"github.com/gorilla/mux"
 )
 
 func assert(err error) {
@@ -27,6 +28,24 @@ func marshal(obj interface{}) []byte {
 	return bytes
 }
 
+func expandVersion(releases []github.RepositoryRelease, version string) string {
+	var release github.RepositoryRelease
+	if version == "latest" {
+		release = releases[0]
+	} else {
+		for _, r := range releases {
+			if r.TagName != nil && *r.TagName == version {
+				release = r
+				break
+			}
+		}
+	}
+	if release.TagName == nil {
+		return ""
+	}
+	return *release.TagName
+}
+
 func main() {
 	port := os.Getenv("PORT")
 	owner := os.Getenv("GITHUB_OWNER")
@@ -37,54 +56,58 @@ func main() {
 
 	client := github.NewClient(t.Client())
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		parts := strings.Split(r.URL.Path, "/")
-		if len(parts) < 4 {
-			http.Error(w, "Not found", http.StatusNotFound)
-			return
-		}
-		repo := parts[1]
-		tag := parts[2]
-		filename := strings.Split(parts[3], ".")
-		releases, _, err := client.Repositories.ListReleases(owner, repo, nil)
+	r := mux.NewRouter()
+
+	r.HandleFunc("/{repo}/latest/version.txt", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		releases, _, err := client.Repositories.ListReleases(owner, vars["repo"], nil)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadGateway)
 			log.Println("error:", err)
 			return
 		}
-		var release github.RepositoryRelease
-		if tag == "latest" {
-			release = releases[0]
-		} else {
-			for _, r := range releases {
-				if r.TagName != nil && *r.TagName == tag {
-					release = r
-					break
-				}
-			}
+		io.WriteString(w, expandVersion(releases, "latest")+"\n")
+	})
+
+	r.HandleFunc("/{repo}/{tag}/{platform}.{ext}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		releases, _, err := client.Repositories.ListReleases(owner, vars["repo"], nil)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadGateway)
+			log.Println("error:", err)
+			return
 		}
-		if release.TagName == nil {
+		version := expandVersion(releases, vars["tag"])
+		if version == "" {
 			http.Error(w, "Not found", http.StatusNotFound)
 			return
 		}
-		tag = *release.TagName
-		version := tag[1:]
-		platform := strings.SplitN(strings.ToLower(filename[0]), "_", 2)
+		platform := strings.SplitN(strings.ToLower(vars["platform"]), "_", 2)
+		if len(platform) < 2 {
+			http.Error(w, "Not found", http.StatusNotFound)
+			return
+		}
 		assetFilename := fmt.Sprintf("%s_%s_%s_%s.%s",
-			repo, version, platform[0], platform[1], filename[1])
+			vars["repo"], version[1:], platform[0], platform[1], vars["ext"])
 		downloadUrl := fmt.Sprintf("https://github.com/%s/%s/releases/download/%s/%s",
-			owner, repo, tag, assetFilename)
+			owner, vars["repo"], version, assetFilename)
 		resp, err := http.Get(downloadUrl)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadGateway)
 			log.Println("error:", err)
 			return
 		}
-		log.Println("download:", downloadUrl)
+		if resp.StatusCode == 200 {
+			log.Println("download:", downloadUrl)
+		} else {
+			log.Println("error:", resp.Status, downloadUrl)
+		}
+		w.WriteHeader(resp.StatusCode)
 		io.Copy(w, resp.Body)
 		resp.Body.Close()
 	})
 
+	http.Handle("/", r)
 	log.Println("serving on port", port, "...")
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
