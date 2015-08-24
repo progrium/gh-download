fdacfb2bbb17eec0cca9faa8ee46ea9938972244
Matt Good <matt@matt-good.net>
2015-08-24 16:01:10 -0700

    Add Keen.io stat tracking

--- gh-download.go
+++ gh-download.go
@@ -5,13 +5,16 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"strings"
+	"time"
 
 	"code.google.com/p/goauth2/oauth"
 	"github.com/google/go-github/github"
 	"github.com/gorilla/mux"
+	"github.com/inconshreveable/go-keen"
 )
 
 func assert(err error) {
@@ -71,6 +74,15 @@ func proxyDownload(w http.ResponseWriter, url string) {
 	resp.Body.Close()
 }
 
+type DownloadEvent struct {
+	Repo            string
+	Tag             string
+	ExpandedVersion string
+	Extension       string
+	Platform        string
+	ClientAddress   string
+}
+
 func main() {
 	port := os.Getenv("PORT")
 	owner := os.Getenv("GITHUB_OWNER")
@@ -80,6 +92,13 @@ func main() {
 	}
 
 	client := github.NewClient(t.Client())
+
+	keenProject := os.Getenv("KEEN_PROJECT")
+	keenWriteKey := os.Getenv("KEEN_WRITE_KEY")
+	keenFlushInterval := 1 * time.Second
+
+	keenClient := &keen.Client{WriteKey: keenWriteKey, ProjectID: keenProject}
+	keenBatchClient := keen.NewBatchClient(keenClient, keenFlushInterval)
 
 	r := mux.NewRouter()
 
@@ -94,61 +113,62 @@ func main() {
 		io.WriteString(w, normalVersion(expandVersion(releases, "latest"))+"\n")
 	})
 
-	r.HandleFunc("/{repo}/{tag}.{ext}", func(w http.ResponseWriter, r *http.Request) {
+	handleDownload := func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
-		releases, _, err := client.Repositories.ListReleases(owner, vars["repo"], nil)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadGateway)
-			log.Println("error:", err)
-			return
-		}
-		version := expandVersion(releases, vars["tag"])
-		w.Header().Set("Version", version)
-		if version == "" {
-			http.Error(w, "Not found", http.StatusNotFound)
-			return
-		}
-		proxyDownload(w, fmt.Sprintf("https://github.com/%s/%s/releases/download/%s/%s",
-			owner,
-			vars["repo"],
-			version,
-			fmt.Sprintf("%s_%s.%s",
-				vars["repo"],
-				version,
-				vars["ext"])))
-	})
 
-	r.HandleFunc("/{repo}/{tag}/{platform}.{ext}", func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		releases, _, err := client.Repositories.ListReleases(owner, vars["repo"], nil)
+		repo := vars["repo"]
+
+		releases, _, err := client.Repositories.ListReleases(owner, repo, nil)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadGateway)
 			log.Println("error:", err)
 			return
 		}
+
 		version := expandVersion(releases, vars["tag"])
 		w.Header().Set("Version", version)
 		if version == "" {
 			http.Error(w, "Not found", http.StatusNotFound)
 			return
 		}
-		w.Header().Set("Platform", vars["platform"])
-		platform := strings.SplitN(strings.ToLower(vars["platform"]), "_", 2)
-		if len(platform) < 2 {
-			http.Error(w, "Not found", http.StatusNotFound)
-			return
+
+		var filenameParts []string
+
+		platform, ok := vars["platform"]
+		if ok {
+			w.Header().Set("Platform", platform)
+			platform = strings.ToLower(platform)
+			if strings.Count(platform, "_") < 1 {
+				http.Error(w, "Not found", http.StatusNotFound)
+				return
+			}
+
+			filenameParts = []string{repo, normalVersion(version), platform}
+		} else {
+			filenameParts = []string{repo, version}
 		}
-		proxyDownload(w, fmt.Sprintf("https://github.com/%s/%s/releases/download/%s/%s",
+
+		if clientAddr, _, err := net.SplitHostPort(r.RemoteAddr); err == nil {
+			keenBatchClient.AddEvent("downloads", &DownloadEvent{
+				Repo:            repo,
+				Tag:             vars["tag"],
+				ExpandedVersion: version,
+				Extension:       vars["ext"],
+				Platform:        platform,
+				ClientAddress:   clientAddr,
+			})
+		}
+
+		proxyDownload(w, fmt.Sprintf("https://github.com/%s/%s/releases/download/%s/%s.%s",
 			owner,
-			vars["repo"],
+			repo,
 			version,
-			fmt.Sprintf("%s_%s_%s_%s.%s",
-				vars["repo"],
-				normalVersion(version),
-				platform[0],
-				platform[1],
-				vars["ext"])))
-	})
+			strings.Join(filenameParts, "_"),
+			vars["ext"]))
+	}
+
+	r.HandleFunc("/{repo}/{tag}.{ext}", handleDownload)
+	r.HandleFunc("/{repo}/{tag}/{platform}.{ext}", handleDownload)
 
 	http.Handle("/", r)
 	log.Println("serving on port", port, "for", owner, "...")
